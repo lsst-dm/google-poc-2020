6e4baa010dc06564b273d4ea3c3795a4dc1fabe2
Kian-Tat Lim <ktl@slac.stanford.edu>
2020-07-27 02:09:29 -0700

    Simplify and enable multiple parallel processes.
    Use conda to get cfitsio/fpack.

--- src/harness.py
+++ src/harness.py
@@ -13,37 +13,31 @@ import socket
 import subprocess
 import tempfile
 import time
-from typing import List
 
 
 def build_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Transfer a stream of CCD images."
     )
-    parser.add_argument('ccd_list', metavar='CCD', nargs='*',
-                        help=("CCD names are in RNN-SNN form"
-                              ", R00-S00 for LATISS"))
     parser.add_argument('-d', '--destination', metavar='URL', required=True,
-                        help=("destination bucket"
-                              " or scp host:path"
-                              " or HTTPS PUT URL"))
+                        help=("URL with"
+                              " gsapi, boto, minio, https, http, bbcp, scp"
+                              " scheme"))
     parser.add_argument('-s', '--starttime', metavar='HH:MM', required=True,
                         help="local time to start simulation")
     parser.add_argument('-n', '--numexp', metavar='EXPOSURES', type=int,
-                        required=True,
-                        help="number of exposures to simulate")
-    parser.add_argument('-c', '--camera', default="AT",
-                        choices=("AT", "CC", "MC"),
-                        help="abbreviation for camera to simulate")
+                        required=True, help="number of exposures to simulate")
+    parser.add_argument('-c', '--ccds', metavar='CCDS', type=int,
+                        default=1, help="number of CCDs to simulate")
     parser.add_argument('-i', '--interval', type=int, default=17,
                         help="interval between exposures in sec")
-    parser.add_argument('-I', '--inputdir', default=".", type=Path,
-                        help="input directory")
-    parser.add_argument('-t', '--tempdir', default="/tmp", type=Path,
+    parser.add_argument('-I', '--inputfile', type=Path,
+                        default="./data/S00.fits", help="input directory")
+    parser.add_argument('-t', '--tempdir', type=Path, default="/tmp",
                         help="temporary directory")
     parser.add_argument('-z', '--compress', action='store_true',
                         help="compress before transfer")
-    parser.add_argument('-p', '--private', action='store_true',
+    parser.add_argument('-P', '--private', action='store_true',
                         help="use private Google Cloud interconnect")
     return parser
 
@@ -65,27 +59,6 @@ class Waiter:
         time.sleep(delay)
 
 
-def find_input(dir: Path, ccd: str) -> Path:
-    # Look for full focal plane
-    input = dir / f"{ccd}.fits"
-    if input.exists():
-        return input
-
-    # Look for single raft
-    if "-" in ccd:
-        raft, sensor = ccd.split("-", 1)
-        input = dir / f"{sensor}.fits"
-        if input.exists():
-            return input
-
-    # Look for single sensor
-    input = dir / "S00.fits"
-    if input.exists():
-        return input
-
-    raise RuntimeError(f"No CCD file found for {ccd}")
-
-
 def log_timing(func):
 
     @functools.wraps(func)
@@ -105,15 +78,12 @@ def log_timing(func):
 @log_timing
 def copy(source: Path, temp: Path, dest: Path, compress: bool = False) -> Path:
     (temp / dest).parent.mkdir(parents=True, exist_ok=True)
+    logging.info(f"Copying {source} to {temp / dest}")
+    subprocess.run(["cp", f"{source}", f"{temp / dest}"])
     if compress:
-        dest = dest.with_suffix(".fits.gz")
-        logging.info(f"Copying {source} to {temp / dest}")
-        with source.open("rb") as s:
-            with (temp / dest).open("wb") as d:
-                subprocess.run(["gzip", "-1"], stdin=s, stdout=d)
-    else:
-        logging.info(f"Copying {source} to {temp / dest}")
-        subprocess.run(["cp", f"{source}", f"{temp / dest}"])
+        logging.info(f"Compressing {temp / dest}")
+        subprocess.run(["fpack", f"{temp / dest}"])
+        dest = dest.with_suffix(".fits.fz")
     return dest
 
 
@@ -246,24 +216,27 @@ class ScpUploader(Uploader):
 
 
 def simulate(
+    ccd_name: str,
     starttime: str,
-    inputdir: Path,
-    tempdir: Path,
     destination: str,
-    ccd_list: List[str],
-    camera: str,
-    compress: bool,
     interval: int,
-    numexp: int
-):
-    hr, min = starttime.split(":")
-    seqnum_start = int(hr + min) * 10
+    tempdir: Path,
+    numexp: int,
+    inputfile: str,
+    compress: bool,
+) -> None:
+    logging.basicConfig(
+        format=f"{ccd_name}" + " {asctime} {message}",
+        style="{",
+        level="INFO"
+    )
 
-    input_path = {ccd: find_input(inputdir, ccd) for ccd in ccd_list}
+    hour, minute = starttime.split(":")
+    seqnum_start = int(hour + minute) * 10
 
     uploader = Uploader.create(destination)
 
-    waiter = Waiter(int(hr), int(min), interval)
+    waiter = Waiter(int(hour), int(minute), interval)
 
     now = datetime.now()
     obs_day = now.strftime("%Y%m%d")
@@ -275,36 +248,28 @@ def simulate(
         for i in range(numexp):
             waiter.wait_exposure(i)
             seqnum = seqnum_start + i
-            for ccd in ccd_list:
-                source_path = input_path[ccd]
-                dest_path = Path(obs_day_str).joinpath(
-                    f"{obs_day}{seqnum:05d}",
-                    f"{camera}_O_{obs_day}_{seqnum:05d}-{ccd}.fits"
-                )
-                logging.info(f"Copying from {source_path} to"
-                             f" {temp_path / dest_path}"
-                             f" with compress = {compress}")
-                logging.info("Begin copy")
-                dest_path = copy(source_path, temp_path, dest_path, compress)
-                logging.info("End copy")
-                logging.info("Begin transfer")
-                uploader.transfer(temp_path, dest_path)
-                logging.info("End transfer")
+            source_path = inputfile
+
+            dest_path = Path(obs_day_str).joinpath(
+                f"{obs_day}{seqnum:05d}",
+                f"MC_O_{obs_day}_{seqnum:05d}_{ccd_name}.fits"
+            )
+            logging.info(f"Copying from {source_path} to"
+                         f" {temp_path / dest_path}"
+                         f" with compress = {compress}")
+            dest_path = copy(source_path, temp_path, dest_path, compress)
+            uploader.transfer(temp_path, dest_path)
 
 
 def main():
     if "APXFR_KEY" in os.environ:
+        print("Using APXFR_KEY credentials")
         with open("/root/secret.json", "w") as f:
             print(os.environ["APXFR_KEY"], file=f)
         os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/root/secret.json"
 
     parser = build_parser()
     args = parser.parse_args()
-    logging.basicConfig(
-        format=f"{args.ccd_list}" + " {asctime} {message}",
-        style="{",
-        level="INFO"
-    )
 
     host_name = socket.gethostname()
     node_match = re.search(r'-(\d+)$', host_name)
@@ -321,25 +286,35 @@ def main():
             else:
                 node_num = 0
     if args.private:
+        print("Using private network")
         with open("/etc/hosts", "a") as f:
             print(f"199.36.153.{int(node_num) % 4 + 8} storage.googleapis.com",
                   file=f)
-    if not args.ccd_list:
-        args.ccd_list = [f"S{node_num}"]
 
-    simulate(
-        args.starttime,
-        args.inputdir,
-        args.tempdir,
-        args.destination,
-        args.ccd_list,
-        args.camera,
-        args.compress,
-        args.interval,
-        args.numexp,
-    )
+    jobs = []
+    for ccd in range(args.ccds):
+        pid = os.fork()
+        if pid == 0:
+            simulate(
+                f"{node_num}-{ccd}",
+                args.starttime,
+                args.destination,
+                args.interval,
+                args.tempdir,
+                args.numexp,
+                args.inputfile,
+                args.compress
+            )
+            logging.info("Child process exiting")
+            exit(0)
+        else:
+            jobs.append(pid)
+    for job in jobs:
+        os.waitpid(job, 0)
 
+    print("Main process sleeping")
     while True:
+        time.sleep(1000000)
         pass
 
 
