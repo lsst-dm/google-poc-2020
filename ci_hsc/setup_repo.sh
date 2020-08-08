bb278c84f994f238c6a974ed34dfb39b187952d7
Hsin-Fang Chiang <hchiang@lsst.org>
2020-08-08 00:32:04 +0000

    Update for recent interface changes in setup_repo

--- ci_hsc/setup_repo.sh
+++ ci_hsc/setup_repo.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
+export BUCKET=bucket_w_2020_30
+export S3_ENDPOINT_URL=https://storage.googleapis.com
 # Need a configured butler.yaml 
-butler create s3://bucket_w_2020_22 -C butler.yaml --override
-butler register-instrument s3://bucket_w_2020_22 -i lsst.obs.subaru.HyperSuprimeCam
-butler write-curated-calibrations s3://bucket_w_2020_22  -i lsst.obs.subaru.HyperSuprimeCam --output-run calib/hsc
+butler create s3://$BUCKET --seed-config  butler.yaml --override
+butler register-instrument s3://$BUCKET lsst.obs.subaru.HyperSuprimeCam
+butler write-curated-calibrations s3://$BUCKET  -i HSC
 
-makeGen3Skymap.py s3://bucket_w_2020_22 -C $CI_HSC_GEN3_DIR/configs/skymap.py skymaps
-butler ingest-raws s3://bucket_w_2020_22 -d $TESTDATA_CI_HSC_DIR/raw --output-run raw/hsc
+makeGen3Skymap.py s3://$BUCKET -C $CI_HSC_GEN3_DIR/configs/skymap.py skymaps
+butler ingest-raws s3://$BUCKET -d $TESTDATA_CI_HSC_DIR/raw
 
-# These will be replaced by butler command lines since w_2020_23
-$CI_HSC_GEN3_DIR/bin.src/defineVisits.py . 
-$CI_HSC_GEN3_DIR/bin.src/ingestExternalData.py . $CI_HSC_GEN3_DIR/resources/external.yaml
+butler define-visits s3://$BUCKET -i HSC --collections HSC/raw/all
+butler import s3://$BUCKET $TESTDATA_CI_HSC_DIR --export-file $CI_HSC_GEN3_DIR/resources/external.yaml --output-run shared/ci_hsc
