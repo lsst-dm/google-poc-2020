d57eeea97b851bce67a80fe9191fa086132a6999
Hsin-Fang Chiang <hchiang@lsst.org>
2020-10-16 19:55:53 +0000

    Update for DM-26874 instrument argument change
    https://github.com/lsst/ci_hsc_gen3/commit/e512d4ffab8bf13ce773a73c12fddee4fcf24d1f

--- drp/ci_hsc/setup_repo.sh
+++ drp/ci_hsc/setup_repo.sh
@@ -5,10 +5,10 @@ export S3_ENDPOINT_URL=https://storage.googleapis.com
 # Need a configured butler.yaml 
 butler create s3://$BUCKET --seed-config  butler.yaml --override
 butler register-instrument s3://$BUCKET lsst.obs.subaru.HyperSuprimeCam
-butler write-curated-calibrations s3://$BUCKET  -i HSC
+butler write-curated-calibrations s3://$BUCKET  HSC
 
 makeGen3Skymap.py s3://$BUCKET -C $CI_HSC_GEN3_DIR/configs/skymap.py skymaps
 butler ingest-raws s3://$BUCKET $TESTDATA_CI_HSC_DIR/raw
 
-butler define-visits s3://$BUCKET -i HSC --collections HSC/raw/all
+butler define-visits s3://$BUCKET HSC --collections HSC/raw/all
 butler import s3://$BUCKET $TESTDATA_CI_HSC_DIR --export-file $CI_HSC_GEN3_DIR/resources/external.yaml
